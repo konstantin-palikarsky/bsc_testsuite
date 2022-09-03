19df23363f7e3ab335fbbab107e06447aa92c9d9
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2022-09-03 16:39:57 +0300

    added time based while loop for workflow simulation

--- src/main/java/testsuite/Main.java
+++ src/main/java/testsuite/Main.java
@@ -7,27 +7,24 @@ import testsuite.workflows.AuthWorkflow;
 import testsuite.workflows.ReadingWorkflow;
 import testsuite.workflows.WritingWorkflow;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         ThesisApi api = new MonolithApi();
-
-        /*TokenDto auth = new TokenDto();
-        auth.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ilx0zac477-977-977-9IiwiZXhwIjoxNjYyMjE0ODQzfQ.5kMkGSI2rbajLDUqG1tHVH-H9MY8kUjaxb29b4boGro");
-        */
-
 
         try {
             AuthWorkflow authenticate = new AuthWorkflow(api);
             var auth = authenticate.execute();
 
+            var authTime = LocalDateTime.now();
             ReadingWorkflow read = new ReadingWorkflow(api);
             WritingWorkflow write = new WritingWorkflow(api, auth);
 
-            for (int i = 0; i < 10; i++) {
-
+            while (LocalDateTime.now().isBefore(authTime.plusSeconds(5))) {
                 read.execute();
                 write.execute();
-
             }
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
